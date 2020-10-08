925a7070248296a09ffab4db60ac32107947e118
Abdul Raza Shaikh <53976455+shaikhraza@users.noreply.github.com>
2020-10-08 22:26:51 +0530

    Update site.js

--- site.js
+++ site.js
@@ -2,7 +2,37 @@
 let questions = [
   {
     id: 1,
-    question: "What is the full form of RAM ?",
+    question: "Java Program To Check Leap Year 
+    public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int year = 1900;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                // year is divisible by 400, hence the year is a leap year
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
+  ",
     answer: "Random Access Memory",
     options: [
       "Random Access Memory",
