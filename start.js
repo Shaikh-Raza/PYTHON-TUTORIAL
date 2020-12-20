25befc708546f3bf4da692f2b3f4418b63342258
Abdul Raza Shaikh <53976455+shaikhraza@users.noreply.github.com>
2020-12-20 12:13:59 +0530

    Delete start.js

--- start.js
+++
@@ -1,8 +0,0 @@
-function submitForm(e) {
-    e.preventDefault();
-    let name = document.forms["welcome_form"]["name"].value;
-  
-    sessionStorage.setItem("name", name);
-  
-    location.href = "quiz.html";
-  }
